18462d5fe5b87afb1a9e85d7f999a3d2200b4882
Oliver Allen <me@oliverallen.co.uk>
2017-05-21 18:35:03 +0100

    escape code & input

--- src/main/java/me/okx/neimonline/FrontendHandler.java
+++ src/main/java/me/okx/neimonline/FrontendHandler.java
@@ -2,6 +2,7 @@ package me.okx.neimonline;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.unbescape.html.HtmlEscape;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -16,8 +17,8 @@ public class FrontendHandler implements HttpHandler {
     public void handle(HttpExchange ex) throws IOException {
         Map<String, String> params = getParams(ex.getRequestURI().getQuery());
 
-        String code = params.getOrDefault("code", "");
-        String input = params.getOrDefault("input", "");
+        String code = HtmlEscape.escapeHtml5(params.getOrDefault("code", ""));
+        String input = HtmlEscape.escapeHtml5(params.getOrDefault("input", ""));
 
         String html = "<!DOCTYPE html><script src=https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js></script><script>$(document).ready(function(){$(\".submit\").click(function(){$(this);var o,n=encodeURIComponent($(\".code\").val()),t=encodeURIComponent($(\".input\").val());o=\"\"==t?\"\":\"&input=\"+t;var e=Date.now(),c=$(\".output\");c.css(\"cursor\",\"progress\"),$.get(\"/api/neim?code=\"+n+o,function(o){var n=Date.now();$(\".timer\").html(\"Took \"+(n-e)+\"ms.\"),c.val(o),c.css(\"cursor\",\"auto\")})}),$(\".link\").click(function(){var o=encodeURIComponent($(\".code\").val()),n=encodeURIComponent($(\".input\").val()),t=\"http://\"+window.location.hostname+\":80\"+window.location.pathname+\"?code=\"+o+\"&input=\"+n;window.prompt(\"Copy to clipboard: Ctrl+C, Enter\",t)})})</script><span>Code:</span><br><textarea class=code cols=32 rows=8>"+code+"</textarea><br><span>Input:</span><br><textarea class=input cols=32 rows=8>"+input+"</textarea><br><button class=submit type=button>Submit</button> <button class=link type=button>Permalink</button><br><span>Output:</span><br><textarea class=output cols=32 rows=8 readonly></textarea><br><span class=timer></span>";
         ex.sendResponseHeaders(200, html.length());
